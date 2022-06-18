a75e812a8aff89db8946f052952de612f31f028a
fionlim <e0857418@u.nus.edu>
2022-06-18 10:47:23 +0800

    products screens and elements added

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, collection, getDocs } from "firebase/auth";
+import { collection, getDocs } from "firebase/auth";
+import { getFirestore } from "firebase/firestore" 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -34,5 +35,5 @@ async function getUsers(db) {
 
 // SDKs
 export const authentication = getAuth(app);
-export 
+
 
