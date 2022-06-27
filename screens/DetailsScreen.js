4d8d1d12e0e87cb7f335d4e70fbf939b14d7d205
fionlim <e0857418@u.nus.edu>
2022-06-27 20:35:49 +0800

    tried changing details screen

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,304 +1,304 @@
-import { useNavigation } from "@react-navigation/core";
-import React, { useState, useEffect } from "react";
-import { View, Image, Text, StyleSheet } from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons";
-import COLORS from "../consts/colors";
-import { TouchableOpacity } from "react-native-gesture-handler";
-//import products from '../consts/products';
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, {useState, useEffect} from 'react';
 import {
-  collection,
-  query,
-  where,
-  getDocs,
-  doc,
-  getDoc,
-} from "firebase/firestore";
-import { firestore } from "../firebase/firebase-config";
+  View,
+  Text,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+  Image,
+  Dimensions,
+  Animated,
+  ToastAndroid,
+} from 'react-native';
+import COLORS from '../consts/colors';
+import products from '../consts/products';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import CartsDropdown from "../components/CartsDropdown";
+//import CartContext from "../CartContext.js";
+//import { render } from 'react-dom';
 
-const DetailsScreen = ({ navigation, route }) => {
-  //const navigation = useNavigation();
-  //const route = useRoute();
+const DetailsScreen = ({route, navigation}) => {
   const product = route.params;
-  console.log(product.id);
-  /*
-  const addNewOrder = async (cartID, productID) => {
-    const cartsRef = doc(firestore, "carts", cartID);
-    try {
-      if (!cartsRef) {
-        console.log("try entered with: " + user.uid);
-        await setDoc(doc(firestore, "carts", cartID), {
-          productsID: [productID],
-          sharersID: [user.id],
-        });
-        console.log("user data added");
-      } else {
-        await updateDoc(cartsRef, {
-          productsID: arrayUnion(productID),
-        });
-      }
-    } catch (error) {
-      console.log("Error in creating order", error);
-    }
-  };*/
 
-  //add to cart function using async
-  const addToCart = async (id) => {
-    let itemArray = await AsyncStorage.getItem("cartItems");
-    itemArray = JSON.parse(itemArray);
-    if (itemArray) {
-      let array = itemArray;
-      array.push(id);
-      //itemArray = array;
+  const [item, setProduct] = useState({});
 
-      try {
-        await AsyncStorage.setItem("cartItems", JSON.stringify(array));
-        ToastAndroid.show(
-          "Item Added Successfully to cart",
-          ToastAndroid.SHORT
-        );
-        navigation.navigate("Quickshop");
-      } catch (error) {
-        return error;
-      }
-    } else {
-      let array = [];
-      array.push(id);
-      try {
-        await AsyncStorage.setItem("cartItems", JSON.stringify(array));
-        ToastAndroid.show(
-          "Item Added Successfully to cart",
-          ToastAndroid.SHORT
-        );
-        navigation.navigate("Quickshop");
-      } catch (error) {
-        return error;
-      }
-    }
-  };
+  const width = Dimensions.get('window').width;
 
-  /*
-  const addToCart = async (id) => {
-    let itemArray = await AsyncStorage.getItem("cartItems");
-    itemArray = JSON.parse(itemArray);
-    if (itemArray) {
-      let array = itemArray;
-      array.push(id);
+  const scrollX = new Animated.Value(0);
 
-      try {
-        await AsyncStorage.setItem("cartItems", JSON.stringify(array));
-        ToastAndroid.show(
-          "Item Added Successfully to cart",
-          ToastAndroid.SHORT
-        );
-        navigation.navigate("Quickshop");
-      } catch (error) {
-        return error;
-      }
-    } else {
-      let array = [];
-      array.push(id);
-      try {
-        await AsyncStorage.setItem("cartItems", JSON.stringify(array));
-        ToastAndroid.show(
-          "Item Added Successfully to cart",
-          ToastAndroid.SHORT
-        );
-        navigation.navigate("Quickshop");
-      } catch (error) {
-        return error;
-      }
-    }
-  };
-  */
-  // prettier-ignore
+  let position = Animated.divide(scrollX, width);
+
+  //const {items, addItemToCart, getTotalPrice} = useContext(CartContext);
+  //get product data by productID
+
+
+  //product horizontal scroll product card
+
   return (
     <View
       style={{
-        flex: 1,
+        width: '100%',
+        height: '100%',
         backgroundColor: COLORS.white,
-      }}
-    >
-      <View style={styles.header}>
-        <Icon name="arrow-back" size={28} onPress={() => navigation.goBack()} />
-        <Icon
-          name="shopping-cart"
-          size={28}
-          onPress={() => navigation.navigate("Cart")}
-        />
-      </View>
-      <View style={styles.imageContainer}>
-        <Image
-          source={product.img}
-          style={{ width: 300, height: 300, resizeMode: "contain", flex: 1 }}
-        />
-      </View>
-      <View style={styles.detailsContainer}>
+        position: 'relative',
+      }}>
+      <StatusBar
+        backgroundColor={COLORS.backgroundLight}
+        barStyle="dark-content"
+      />
+      <ScrollView>
         <View
           style={{
-            marginLeft: 20,
-            flexDirection: "row",
-            alignItems: "flex-end",
-          }}
-        >
-          <View style={styles.line} />
-          <Text style={{ fontSize: 18, fontWeight: "bold" }}>Best choice</Text>
-        </View>
-        <View
-          style={{
-            marginLeft: 20,
-            marginTop: 20,
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Text style={{ fontSize: 22, fontWeight: "bold" }}>
-            {product.name}
-          </Text>
-          <View style={styles.priceTag}>
-            <Text
-              style={{
-                marginLeft: 15,
-                color: COLORS.white,
-                fontWeight: "bold",
-                fontSize: 16,
-              }}
-            >
-              ${product.price}
-            </Text>
+            width: '100%',
+            backgroundColor: COLORS.backgroundLight,
+            borderBottomRightRadius: 20,
+            borderBottomLeftRadius: 20,
+            position: 'relative',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: 4,
+          }}>
+          <View
+            style={{
+              width: '100%',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingTop: 16,
+              paddingLeft: 16,
+            }}>
+            <TouchableOpacity onPress={() => navigation.goBack('Home')}>
+              <Entypo
+                name="chevron-left"
+                style={{
+                  fontSize: 18,
+                  color: COLORS.backgroundDark,
+                  padding: 12,
+                  backgroundColor: COLORS.white,
+                  borderRadius: 10,
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+          <FlatList
+            data={product.productImageList ? product.productImageList : null}
+            horizontal
+            renderItem={renderProduct}
+            showsHorizontalScrollIndicator={false}
+            decelerationRate={0.8}
+            snapToInterval={width}
+            bounces={false}
+            onScroll={Animated.event(
+              [{nativeEvent: {contentOffset: {x: scrollX}}}],
+              {useNativeDriver: false},
+            )}
+          />
+          <View
+            style={{
+              width: '100%',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginBottom: 16,
+              marginTop: 32,
+            }}>
+            {product.productImageList
+              ? product.productImageList.map((data, index) => {
+                  let opacity = position.interpolate({
+                    inputRange: [index - 1, index, index + 1],
+                    outputRange: [0.2, 1, 0.2],
+                    extrapolate: 'clamp',
+                  });
+                  return (
+                    <Animated.View
+                      key={index}
+                      style={{
+                        width: '16%',
+                        height: 2.4,
+                        backgroundColor: COLORS.black,
+                        opacity,
+                        marginHorizontal: 4,
+                        borderRadius: 100,
+                      }}></Animated.View>
+                  );
+                })
+              : null}
           </View>
         </View>
-        <View style={{paddingHorizontal: 20, marginTop: 10}}>
-          <Text style={{fontSize: 20, fontWeight: 'bold'}}>About</Text>
+        <View
+          style={{
+            paddingHorizontal: 16,
+            marginTop: 6,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginVertical: 14,
+            }}>
+            <Entypo
+              name="shopping-cart"
+              style={{
+                fontSize: 18,
+                color: COLORS.blue,
+                marginRight: 6,
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 12,
+                color: COLORS.black,
+              }}>
+              Shopping
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginVertical: 4,
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Text
+              style={{
+                fontSize: 24,
+                fontWeight: '600',
+                letterSpacing: 0.5,
+                marginVertical: 4,
+                color: COLORS.black,
+                maxWidth: '84%',
+              }}>
+              {product.name}
+            </Text>
+            <Ionicons
+              name="link-outline"
+              style={{
+                fontSize: 24,
+                color: COLORS.blue,
+                backgroundColor: COLORS.blue + 10,
+                padding: 8,
+                borderRadius: 100,
+              }}
+            />
+          </View>
           <Text
             style={{
-              color: 'grey',
-              fontSize: 16,
-              lineHeight: 22,
-              marginTop: 10,
+              fontSize: 12,
+              color: COLORS.black,
+              fontWeight: '400',
+              letterSpacing: 1,
+              opacity: 0.5,
+              lineHeight: 20,
+              maxWidth: '85%',
+              maxHeight: 44,
+              marginBottom: 18,
             }}>
             {product.about}
           </Text>
           <View
             style={{
-              marginTop: 20,
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              marginVertical: 14,
+              borderBottomColor: COLORS.backgroundLight,
+              borderBottomWidth: 1,
+              paddingBottom: 20,
+            }}>
             <View
               style={{
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <View style={styles.borderBtn}>
-                <Text style={styles.borderBtnText}>-</Text>
-              </View>
-              <Text
+                flexDirection: 'row',
+                width: '80%',
+                alignItems: 'center',
+              }}>
+              <View
                 style={{
-                  fontSize: 20,
-                  marginHorizontal: 10,
-                  fontWeight: "bold",
-                }}
-              >
-                1
-              </Text>
-              <View style={styles.borderBtn}>
-                <Text style={styles.borderBtnText}>+</Text>
+                  color: COLORS.blue,
+                  backgroundColor: COLORS.backgroundLight,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  padding: 12,
+                  borderRadius: 100,
+                  marginRight: 10,
+                }}>
+                <Entypo
+                  name="location-pin"
+                  style={{
+                    fontSize: 16,
+                    color: COLORS.blue,
+                  }}
+                />
               </View>
+              <Text> Address</Text>
             </View>
-
-            <View style={styles.buyBtn}>
-            <CartsDropdown productID = {product.id}/> 
-              
-            </View>
+            <Entypo
+              name="chevron-right"
+              style={{
+                fontSize: 22,
+                color: COLORS.backgroundDark,
+              }}
+            />
           </View>
-          
+          <View
+            style={{
+              paddingHorizontal: 16,
+            }}>
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: '500',
+                maxWidth: '85%',
+                color: COLORS.black,
+                marginBottom: 4,
+              }}>
+              ${product.price}.00
+            </Text>
+            <Text>
+              ${product.price / 20} (&#8377;
+              {product.price + product.price / 20})
+            </Text>
+          </View>
         </View>
+      </ScrollView>
+       <CartsDropdown productID = {product.id}/> 
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 10,
+          height: '8%',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+       
       </View>
     </View>
   );
 };
-// what's the product ID ah
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header: {
-    paddingHorizontal: 20,
-    marginTop: 20,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  imageContainer: {
-    flex: 0.45,
-    marginTop: 20,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  detailsContainer: {
-    flex: 0.55,
-    backgroundColor: COLORS.light,
-    marginHorizontal: 7,
-    marginBottom: 7,
-    borderRadius: 20,
-    marginTop: 30,
-    paddingTop: 30,
-  },
-  line: {
-    width: 25,
-    height: 2,
-    backgroundColor: COLORS.dark,
-    marginBottom: 5,
-    marginRight: 3,
-  },
-  borderBtn: {
-    borderColor: "grey",
-    borderWidth: 1,
-    borderRadius: 5,
-    justifyContent: "center",
-    alignItems: "center",
-    width: 60,
-    height: 40,
-  },
-  borderBtnText: { fontWeight: "bold", fontSize: 28 },
-  buyBtn: {
-    width: 130,
-    height: 50,
-    backgroundColor: COLORS.green,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 30,
-  },
-  priceTag: {
-    backgroundColor: COLORS.green,
-    width: 80,
-    height: 40,
-    justifyContent: "center",
-    borderTopLeftRadius: 25,
-    borderBottomLeftRadius: 25,
-  },
-  dropdown: {
-    backgroundColor: "white",
-    borderBottomColor: "gray",
-    borderBottomWidth: 0.5,
-    marginTop: 20,
-  },
-});
 
 export default DetailsScreen;
 
-/* fion's add to cart button
-<TouchableOpacity
-                onPress={() => addNewOrder(product.id)} // to test if navigation working
-                style={styles.buyBtn}
-              >
-                <Text style={{ fontSize: 20, color: "white" }}>
-                  add to cart
-                </Text>
-              </TouchableOpacity>
-              */
+// original add-to-cart-button
+ /*<TouchableOpacity
+          onPress={() => (product.isAvailable ? addToCart(product.id) : null)}
+          style={{
+            width: '86%',
+            height: '90%',
+            backgroundColor: COLORS.blue,
+            borderRadius: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 12,
+              fontWeight: '500',
+              letterSpacing: 1,
+              color: COLORS.white,
+              textTransform: 'uppercase',
+            }}>
+            {product.isAvailable ? 'Add to cart' : 'Not Available'}
+          </Text>
+        </TouchableOpacity>*/
---
+++ screens/DetailsScreen1.js
@@ -0,0 +1,304 @@
+import { useNavigation } from "@react-navigation/core";
+import React, { useState, useEffect } from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import COLORS from "../consts/colors";
+import { TouchableOpacity } from "react-native-gesture-handler";
+//import products from '../consts/products';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  getDoc,
+} from "firebase/firestore";
+import { firestore } from "../firebase/firebase-config";
+import CartsDropdown from "../components/CartsDropdown";
+
+const DetailsScreen = ({ navigation, route }) => {
+  //const navigation = useNavigation();
+  //const route = useRoute();
+  const product = route.params;
+  console.log(product.id);
+  /*
+  const addNewOrder = async (cartID, productID) => {
+    const cartsRef = doc(firestore, "carts", cartID);
+    try {
+      if (!cartsRef) {
+        console.log("try entered with: " + user.uid);
+        await setDoc(doc(firestore, "carts", cartID), {
+          productsID: [productID],
+          sharersID: [user.id],
+        });
+        console.log("user data added");
+      } else {
+        await updateDoc(cartsRef, {
+          productsID: arrayUnion(productID),
+        });
+      }
+    } catch (error) {
+      console.log("Error in creating order", error);
+    }
+  };*/
+
+  //add to cart function using async
+  const addToCart = async (id) => {
+    let itemArray = await AsyncStorage.getItem("cartItems");
+    itemArray = JSON.parse(itemArray);
+    if (itemArray) {
+      let array = itemArray;
+      array.push(id);
+      //itemArray = array;
+
+      try {
+        await AsyncStorage.setItem("cartItems", JSON.stringify(array));
+        ToastAndroid.show(
+          "Item Added Successfully to cart",
+          ToastAndroid.SHORT
+        );
+        navigation.navigate("Quickshop");
+      } catch (error) {
+        return error;
+      }
+    } else {
+      let array = [];
+      array.push(id);
+      try {
+        await AsyncStorage.setItem("cartItems", JSON.stringify(array));
+        ToastAndroid.show(
+          "Item Added Successfully to cart",
+          ToastAndroid.SHORT
+        );
+        navigation.navigate("Quickshop");
+      } catch (error) {
+        return error;
+      }
+    }
+  };
+
+  /*
+  const addToCart = async (id) => {
+    let itemArray = await AsyncStorage.getItem("cartItems");
+    itemArray = JSON.parse(itemArray);
+    if (itemArray) {
+      let array = itemArray;
+      array.push(id);
+
+      try {
+        await AsyncStorage.setItem("cartItems", JSON.stringify(array));
+        ToastAndroid.show(
+          "Item Added Successfully to cart",
+          ToastAndroid.SHORT
+        );
+        navigation.navigate("Quickshop");
+      } catch (error) {
+        return error;
+      }
+    } else {
+      let array = [];
+      array.push(id);
+      try {
+        await AsyncStorage.setItem("cartItems", JSON.stringify(array));
+        ToastAndroid.show(
+          "Item Added Successfully to cart",
+          ToastAndroid.SHORT
+        );
+        navigation.navigate("Quickshop");
+      } catch (error) {
+        return error;
+      }
+    }
+  };
+  */
+  // prettier-ignore
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+      }}
+    >
+      <View style={styles.header}>
+        <Icon name="arrow-back" size={28} onPress={() => navigation.goBack()} />
+        <Icon
+          name="shopping-cart"
+          size={28}
+          onPress={() => navigation.navigate("Cart")}
+        />
+      </View>
+      <View style={styles.imageContainer}>
+        <Image
+          source={product.img}
+          style={{ width: 300, height: 300, resizeMode: "contain", flex: 1 }}
+        />
+      </View>
+      <View style={styles.detailsContainer}>
+        <View
+          style={{
+            marginLeft: 20,
+            flexDirection: "row",
+            alignItems: "flex-end",
+          }}
+        >
+          <View style={styles.line} />
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>Best choice</Text>
+        </View>
+        <View
+          style={{
+            marginLeft: 20,
+            marginTop: 20,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 22, fontWeight: "bold" }}>
+            {product.name}
+          </Text>
+          <View style={styles.priceTag}>
+            <Text
+              style={{
+                marginLeft: 15,
+                color: COLORS.white,
+                fontWeight: "bold",
+                fontSize: 16,
+              }}
+            >
+              ${product.price}
+            </Text>
+          </View>
+        </View>
+        <View style={{paddingHorizontal: 20, marginTop: 10}}>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>About</Text>
+          <Text
+            style={{
+              color: 'grey',
+              fontSize: 16,
+              lineHeight: 22,
+              marginTop: 10,
+            }}>
+            {product.about}
+          </Text>
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <View style={styles.borderBtn}>
+                <Text style={styles.borderBtnText}>-</Text>
+              </View>
+              <Text
+                style={{
+                  fontSize: 20,
+                  marginHorizontal: 10,
+                  fontWeight: "bold",
+                }}
+              >
+                1
+              </Text>
+              <View style={styles.borderBtn}>
+                <Text style={styles.borderBtnText}>+</Text>
+              </View>
+            </View>
+
+            <View style={styles.buyBtn}>
+            <CartsDropdown productID = {product.id}/> 
+              
+            </View>
+          </View>
+          
+        </View>
+      </View>
+    </View>
+  );
+};
+// what's the product ID ah
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    paddingHorizontal: 20,
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  imageContainer: {
+    flex: 0.45,
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  detailsContainer: {
+    flex: 0.55,
+    backgroundColor: COLORS.light,
+    marginHorizontal: 7,
+    marginBottom: 7,
+    borderRadius: 20,
+    marginTop: 30,
+    paddingTop: 30,
+  },
+  line: {
+    width: 25,
+    height: 2,
+    backgroundColor: COLORS.dark,
+    marginBottom: 5,
+    marginRight: 3,
+  },
+  borderBtn: {
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 60,
+    height: 40,
+  },
+  borderBtnText: { fontWeight: "bold", fontSize: 28 },
+  buyBtn: {
+    width: 130,
+    height: 50,
+    backgroundColor: COLORS.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+  },
+  priceTag: {
+    backgroundColor: COLORS.green,
+    width: 80,
+    height: 40,
+    justifyContent: "center",
+    borderTopLeftRadius: 25,
+    borderBottomLeftRadius: 25,
+  },
+  dropdown: {
+    backgroundColor: "white",
+    borderBottomColor: "gray",
+    borderBottomWidth: 0.5,
+    marginTop: 20,
+  },
+});
+
+export default DetailsScreen;
+
+/* fion's add to cart button
+<TouchableOpacity
+                onPress={() => addNewOrder(product.id)} // to test if navigation working
+                style={styles.buyBtn}
+              >
+                <Text style={{ fontSize: 20, color: "white" }}>
+                  add to cart
+                </Text>
+              </TouchableOpacity>
+              */
