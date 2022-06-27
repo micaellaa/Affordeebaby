7f45ccf9cb09f0939b50f69dc2bc88b7bdf94597
micaellaa <99943347+micaellaa@users.noreply.github.com>
2022-06-27 16:28:55 +0800

    Cart Screen partially done
    still some issues with rendering

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -7,6 +7,12 @@ import {
   TouchableOpacity,
   Image,
   ToastAndroid,
+  Dimensions,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TextInput,
+  FlatList,
+  SafeAreaView,
 } from "react-native";
 import COLORS from "../consts/colors";
 import products from "../consts/products";
@@ -15,10 +21,13 @@ import { doc, getDoc } from "firebase/firestore";
 import { firestore } from "../firebase/firebase-config";
 //import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const CartScreen = (cartID) => {
+const width = Dimensions.get("window").width / 2 - 30;
+
+const CartScreen = ({ navig, route }) => {
   const navigation = useNavigation();
-  const [product, setProduct] = useState();
-  const [total, setTotal] = useState(null);
+  const cartID = route.params;
+  //const [product, setProduct] = useState();
+  //const [total, setTotal] = useState(null);
 
   const [productsID, setProductsID] = useState([]);
   const cartsRef = doc(firestore, "carts", cartID);
@@ -40,249 +49,159 @@ const CartScreen = (cartID) => {
     fetchCartData();
   }, []);
 
+  console.log("productsID: ", productsID);
+
   let productData = [];
 
   //productsID contains list of product ID's inside the cart
-  /* if (productsID) {
+  if (productsID) {
     productsID.forEach((productID) => {
       //if (items.includes(product.id)) {
-      console.log(productID.productID);
+      console.log(productID);
       const product = products.find(
         (element) => element.id == productID.productID
       );
       productData.push(product);
-      //return;
     });
-    setProduct(productData);
+    //setProduct(productData);
     //getTotal(productData);
-  } else {
-    setProduct(false);
-    //getTotal(false);
-  }*/
+  }
 
-  /*
-  //get total price of all items in the cart
-  const getTotal = (productData) => {
+  console.log(productData);
 
-    for (let index = 0; index < productData.length; index++) {
-      let productPrice = productData[index].price;
-      total = total + productPrice;
+  const Card = ({ product }) => {
+    if (!product) {
+      console.log("product is null");
+      return null;
     }
+    const [name, setName] = useState("");
+    //get cart name
 
-    setTotal(0);
-  };
-
-  const checkOut = async () => {
-    navigation.navigate("Home");
-  };
-
-  const renderProducts = (product) => {
+    setName(product.name);
     return (
       <TouchableOpacity
-        //key={product.key}
-        onPress={() =>
-          navigation.navigate("DetailsScreen", { productID: product.id })
-        }
-        style={{
-          width: "100%",
-          height: 100,
-          marginVertical: 6,
-          flexDirection: "row",
-          alignItems: "center",
-        }}
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate("Details", product)}
       >
-        <View
-          style={{
-            width: "30%",
-            height: 100,
-            padding: 14,
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: COLORS.light,
-            borderRadius: 10,
-            marginRight: 22,
-          }}
-        >
-          <Image
-            source={product.img}
-            style={{
-              width: "100%",
-              height: "100%",
-              resizeMode: "contain",
-            }}
-          />
-        </View>
-        <View
-          style={{
-            flex: 1,
-            height: "100%",
-            justifyContent: "space-around",
-          }}
-        >
-          <View style={{}}>
-            <Text
-              style={{
-                fontSize: 14,
-                maxWidth: "100%",
-                color: COLORS.dark,
-                fontWeight: "600",
-                letterSpacing: 1,
-              }}
-            >
-              {product.name}
-            </Text>
-            <View
-              style={{
-                marginTop: 4,
-                flexDirection: "row",
-                alignItems: "center",
-                opacity: 0.6,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 14,
-                  fontWeight: "400",
-                  maxWidth: "85%",
-                  marginRight: 4,
-                }}
-              >
-                &#8377;{product.price}
-              </Text>
-              <Text>
-                (~&#8377;
-                {product.price + product.price / 20})
-              </Text>
-            </View>
-          </View>
+        <View style={styles.card}>
+          <Text style={{ fontWeight: "bold", fontSize: 17, marginTop: 10 }}>
+            {name}
+          </Text>
           <View
             style={{
               flexDirection: "row",
               justifyContent: "space-between",
-              alignItems: "center",
+              marginTop: 5,
             }}
           >
             <View
               style={{
-                flexDirection: "row",
+                height: 25,
+                width: 50,
+                backgroundColor: COLORS.indigo,
+                borderRadius: 5,
+                justifyContent: "center",
                 alignItems: "center",
               }}
             >
-              <View
+              <Text
                 style={{
-                  borderRadius: 100,
-                  marginRight: 20,
-                  padding: 4,
-                  borderWidth: 1,
-                  borderColor: COLORS.light,
-                  opacity: 0.5,
+                  fontSize: 18,
+                  color: COLORS.white,
+                  fontWeight: "bold",
                 }}
               >
-                <Icon
-                  name="minus"
-                  style={{
-                    fontSize: 16,
-                    color: COLORS.dark,
-                  }}
-                />
-              </View>
-              <Text>1</Text>
-              <View
-                style={{
-                  borderRadius: 100,
-                  marginLeft: 20,
-                  padding: 4,
-                  borderWidth: 1,
-                  borderColor: COLORS.light,
-                  opacity: 0.5,
-                }}
-              >
-                <Icon
-                  name="plus"
-                  style={{
-                    fontSize: 16,
-                    color: COLORS.dark,
-                  }}
-                />
-              </View>
+                Edit
+              </Text>
             </View>
           </View>
         </View>
       </TouchableOpacity>
     );
   };
-*/
-  //final return
+
   return (
-    /*
-    <View
-      style={{
-        width: "100%",
-        height: "100%",
-        backgroundColor: COLORS.white,
-        position: "relative",
-      }}
-    >
-      <ScrollView>
-        <View
-          style={{
-            width: "100%",
-            flexDirection: "row",
-            paddingTop: 16,
-            paddingHorizontal: 16,
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Icon
-              name="chevron-left"
-              style={{
-                fontSize: 18,
-                color: COLORS.dark,
-                padding: 12,
-                backgroundColor: COLORS.light,
-                borderRadius: 12,
-              }}
-            />
-          </TouchableOpacity>
+    <View>
+      <View
+        style={{
+          flex: 3,
+          paddingHorizontal: 50,
+          backgroundColor: COLORS.white,
+        }}
+      >
+        <View>
+          <Text
+            style={{ fontSize: 25, fontWeight: "bold", paddingHorizontal: 50 }}
+          >
+            My
+          </Text>
           <Text
             style={{
-              fontSize: 14,
-              color: COLORS.dark,
-              fontWeight: "400",
+              fontSize: 38,
+              color: COLORS.indigo,
+              fontWeight: "bold",
+              paddingHorizontal: 50,
             }}
           >
-            Order Details
+            Carts
           </Text>
-          <View></View>
         </View>
-        <Text
-          style={{
-            fontSize: 20,
-            color: COLORS.dark,
-            fontWeight: "500",
-            letterSpacing: 1,
-            paddingTop: 20,
-            paddingLeft: 16,
-            marginBottom: 10,
-          }}
-        >
-          My Cart
-        </Text>
-        <View style={{ paddingHorizontal: 16 }}>
-          {product ? product.map(renderProducts) : null}
+      </View>
+      <View
+        style={{ marginTop: 30, flexDirection: "row", paddingHorizontal: 50 }}
+      >
+        <View style={styles.searchContainer}>
+          <Icon name="search" size={25} style={{ marginLeft: 20 }} />
+          <TextInput placeholder="Search" style={styles.input} />
         </View>
-      </ScrollView>
-    </View>
-  );*/
-    <View>
-      <Text>Stub</Text>
+        <View style={styles.sortBtn}>
+          <Icon name="sort" size={30} color={COLORS.white} />
+        </View>
+      </View>
+      <FlatList
+        columnWrapperStyle={{ justifyContent: "space-between" }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          marginTop: 10,
+          paddingBottom: 50,
+        }}
+        numColumns={2}
+        data={productData}
+        renderItem={(item) => {
+          return <Card product={item} />;
+        }}
+      />
     </View>
   );
 };
 
 export default CartScreen;
+
+const styles = StyleSheet.create({
+  categoryContainer: {
+    flexDirection: "row",
+    marginTop: 30,
+    marginBottom: 20,
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  categoryText: { fontSize: 16, color: "grey", fontWeight: "bold" },
+  categoryTextSelected: {
+    color: COLORS.indigo,
+    paddingBottom: 5,
+    borderBottomWidth: 5,
+    borderColor: COLORS.indigo,
+  },
+  card: {
+    height: 225,
+    backgroundColor: COLORS.white,
+    width,
+    marginHorizontal: 2,
+    borderRadius: 10,
+    marginBottom: 20,
+    paddingHorizontal: 15,
+  },
+});
 
 /* fions getData
 const getDataFromDB = async () => {
