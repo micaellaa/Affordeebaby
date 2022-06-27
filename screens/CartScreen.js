0e79a28cbbaf646aac8632592d1eaeb912399e51
micaellaa <99943347+micaellaa@users.noreply.github.com>
2022-06-27 15:07:48 +0800

    Changes in createUserDocument
    for better demonstration purposes for milestone 2

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -8,7 +8,6 @@ import {
   Image,
   ToastAndroid,
 } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import COLORS from "../consts/colors";
 import products from "../consts/products";
 import Icon from "react-native-vector-icons/MaterialIcons";
@@ -21,7 +20,7 @@ const CartScreen = (cartID) => {
   const [product, setProduct] = useState();
   const [total, setTotal] = useState(null);
 
-  /*obtain data from firestore
+  const [productsID, setProductsID] = useState([]);
   const cartsRef = doc(firestore, "carts", cartID);
 
   useEffect(() => {
@@ -33,144 +32,51 @@ const CartScreen = (cartID) => {
       try {
         console.log("try entered");
         const productList = docSnap.get("productsID");
-        setProduct(productList);
+        setProductsID(productList);
       } catch (error) {
         console.log("Error in finding products", error);
       }
     }
     fetchCartData();
   }, []);
-*/
 
-  useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {
-      getDataFromDB();
+  let productData = [];
+
+  //productsID contains list of product ID's inside the cart
+  /* if (productsID) {
+    productsID.forEach((productID) => {
+      //if (items.includes(product.id)) {
+      console.log(productID.productID);
+      const product = products.find(
+        (element) => element.id == productID.productID
+      );
+      productData.push(product);
+      //return;
     });
-    return unsubscribe;
-  }, [navigation]);
+    setProduct(productData);
+    //getTotal(productData);
+  } else {
+    setProduct(false);
+    //getTotal(false);
+  }*/
 
-  //get data here
-  const getDataFromDB = async () => {
-    //let items = await AsyncStorage.getItem("cartItems");
-    //items = JSON.parse(items);
-
-    const [products1, setProducts1] = useState([]);
-    const cartsRef = doc(firestore, "carts", cartID);
-
-    useEffect(() => {
-      async function fetchCartData() {
-        const docSnap = await getDoc(cartsRef);
-        if (docSnap.exists()) {
-          console.log("Document data:", docSnap.data());
-        }
-        try {
-          console.log("try entered");
-          const productList = docSnap.get("productsID");
-          setProducts1(productList);
-        } catch (error) {
-          console.log("Error in finding products", error);
-        }
-      }
-      fetchCartData();
-    }, []);
-
-    let productData = [];
-
-    if (products1) {
-      products1.forEach((product) => {
-        //if (items.includes(product.id)) {
-        productData.push(product);
-        //return;
-      });
-      setProduct(productData);
-      getTotal(productData);
-    } else {
-      setProduct(false);
-      getTotal(false);
-    }
-  };
-
+  /*
   //get total price of all items in the cart
   const getTotal = (productData) => {
-    let total = 0;
+
     for (let index = 0; index < productData.length; index++) {
       let productPrice = productData[index].price;
       total = total + productPrice;
     }
-    setTotal(total);
-  };
 
-  //remove data from Cart
-  /*
-  const removeItemFromCart = async (id) => {
-    let itemArray = await AsyncStorage.getItem("cartItems");
-    itemArray = JSON.parse(itemArray);
-    if (itemArray) {
-      let array = itemArray;
-      for (let index = 0; index < array.length; index++) {
-        if (array[index] == id) {
-          array.splice(index, 1);
-        }
-
-        await AsyncStorage.setItem("cartItems", JSON.stringify(array));
-        getDataFromDB();
-      }
-    }
+    setTotal(0);
   };
-*/
-  //checkout
 
   const checkOut = async () => {
-    try {
-      await AsyncStorage.removeItem("cartItems");
-    } catch (error) {
-      return error;
-    }
-
-    ToastAndroid.show("Items will be Deliverd SOON!", ToastAndroid.SHORT);
-
     navigation.navigate("Home");
   };
 
-  /*
-  const renderProducts = (productID) => {
-    return (
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate("DetailsScreen", { productID: product })
-        }
-        style={{
-          width: "100%",
-          height: 100,
-          marginVertical: 6,
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-      >
-        <View
-          style={{
-            flex: 1,
-            height: "100%",
-            justifyContent: "space-around",
-          }}
-        >
-          <View style={{}}>
-            <Text
-              style={{
-                fontSize: 14,
-                maxWidth: "100%",
-                color: COLORS.dark,
-                fontWeight: "600",
-                letterSpacing: 1,
-              }}
-            >
-              {product.name}
-
-      </TouchableOpacity>
-  }
-  */
-
-  const renderProducts = (product, index) => {
+  const renderProducts = (product) => {
     return (
       <TouchableOpacity
         //key={product.key}
@@ -300,26 +206,15 @@ const CartScreen = (cartID) => {
                 />
               </View>
             </View>
-            <TouchableOpacity onPress={() => removeItemFromCart(product.id)}>
-              <Icon
-                name="delete-outline"
-                style={{
-                  fontSize: 16,
-                  color: COLORS.dark,
-                  backgroundColor: COLORS.light,
-                  padding: 8,
-                  borderRadius: 100,
-                }}
-              />
-            </TouchableOpacity>
           </View>
         </View>
       </TouchableOpacity>
     );
   };
-
+*/
   //final return
   return (
+    /*
     <View
       style={{
         width: "100%",
@@ -378,312 +273,11 @@ const CartScreen = (cartID) => {
         <View style={{ paddingHorizontal: 16 }}>
           {product ? product.map(renderProducts) : null}
         </View>
-        <View>
-          <View
-            style={{
-              paddingHorizontal: 16,
-              marginVertical: 10,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 16,
-                color: COLORS.darkk,
-                fontWeight: "500",
-                letterSpacing: 1,
-                marginBottom: 20,
-              }}
-            >
-              Delivery Location
-            </Text>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-              }}
-            >
-              <View
-                style={{
-                  flexDirection: "row",
-                  width: "80%",
-                  alignItems: "center",
-                }}
-              >
-                <View
-                  style={{
-                    color: COLORS.indigo,
-                    backgroundColor: COLORS.light,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    padding: 12,
-                    borderRadius: 10,
-                    marginRight: 18,
-                  }}
-                >
-                  <Icon
-                    name="truck-delivery-outline"
-                    style={{
-                      fontSize: 18,
-                      color: COLORS.indigo,
-                    }}
-                  />
-                </View>
-                <View>
-                  <Text
-                    style={{
-                      fontSize: 14,
-                      color: COLORS.dark,
-                      fontWeight: "500",
-                    }}
-                  >
-                    2 Petre Melikishvili St.
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 12,
-                      color: COLORS.dark,
-                      fontWeight: "400",
-                      lineHeight: 20,
-                      opacity: 0.5,
-                    }}
-                  >
-                    0162, Tbilisi
-                  </Text>
-                </View>
-              </View>
-              <Icon
-                name="chevron-right"
-                style={{ fontSize: 22, color: COLORS.dark }}
-              />
-            </View>
-          </View>
-          <View
-            style={{
-              paddingHorizontal: 16,
-              marginVertical: 10,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 16,
-                color: COLORS.dark,
-                fontWeight: "500",
-                letterSpacing: 1,
-                marginBottom: 20,
-              }}
-            >
-              Payment Method
-            </Text>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-              }}
-            >
-              <View
-                style={{
-                  flexDirection: "row",
-                  width: "80%",
-                  alignItems: "center",
-                }}
-              >
-                <View
-                  style={{
-                    color: COLORS.indigo,
-                    backgroundColor: COLORS.light,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    padding: 12,
-                    borderRadius: 10,
-                    marginRight: 18,
-                  }}
-                >
-                  <Text
-                    style={{
-                      fontSize: 10,
-                      fontWeight: "900",
-                      color: COLORS.indigo,
-                      letterSpacing: 1,
-                    }}
-                  >
-                    VISA
-                  </Text>
-                </View>
-                <View>
-                  <Text
-                    style={{
-                      fontSize: 14,
-                      color: COLORS.dark,
-                      fontWeight: "500",
-                    }}
-                  >
-                    Visa Classic
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 12,
-                      color: COLORS.dark,
-                      fontWeight: "400",
-                      lineHeight: 20,
-                      opacity: 0.5,
-                    }}
-                  >
-                    ****-9092
-                  </Text>
-                </View>
-              </View>
-              <Icon
-                name="chevron-right"
-                style={{ fontSize: 22, color: COLORS.dark }}
-              />
-            </View>
-          </View>
-          <View
-            style={{
-              paddingHorizontal: 16,
-              marginTop: 40,
-              marginBottom: 80,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 16,
-                color: COLORS.dark,
-                fontWeight: "500",
-                letterSpacing: 1,
-                marginBottom: 20,
-              }}
-            >
-              Order Info
-            </Text>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-                marginBottom: 8,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: "400",
-                  maxWidth: "80%",
-                  color: COLORS.dark,
-                  opacity: 0.5,
-                }}
-              >
-                Subtotal
-              </Text>
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: "400",
-                  color: COLORS.dark,
-                  opacity: 0.8,
-                }}
-              >
-                &#8377;{total}.00
-              </Text>
-            </View>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-                marginBottom: 22,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: "400",
-                  maxWidth: "80%",
-                  color: COLORS.dark,
-                  opacity: 0.5,
-                }}
-              >
-                Shipping Tax
-              </Text>
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: "400",
-                  color: COLORS.dark,
-                  opacity: 0.8,
-                }}
-              >
-                &#8377;{total / 20}
-              </Text>
-            </View>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: "400",
-                  maxWidth: "80%",
-                  color: COLORS.dark,
-                  opacity: 0.5,
-                }}
-              >
-                Total
-              </Text>
-              <Text
-                style={{
-                  fontSize: 18,
-                  fontWeight: "500",
-                  color: COLORS.dark,
-                }}
-              >
-                &#8377;{total + total / 20}
-              </Text>
-            </View>
-          </View>
-        </View>
       </ScrollView>
-
-      <View
-        style={{
-          position: "absolute",
-          bottom: 10,
-          height: "8%",
-          width: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <TouchableOpacity
-          onPress={() => (total != 0 ? checkOut() : null)}
-          style={{
-            width: "86%",
-            height: "90%",
-            backgroundColor: COLORS.indigo,
-            borderRadius: 20,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 12,
-              fontWeight: "500",
-              letterSpacing: 1,
-              color: COLORS.white,
-              textTransform: "uppercase",
-            }}
-          >
-            CHECKOUT (&#8377;{total + total / 20} )
-          </Text>
-        </TouchableOpacity>
-      </View>
+    </View>
+  );*/
+    <View>
+      <Text>Stub</Text>
     </View>
   );
 };
@@ -710,3 +304,48 @@ const getDataFromDB = async () => {
     }
   };
   */
+
+/* getDataFromDB
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      getDataFromDB();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
+  const getDataFromDB = async () => {
+    //let items = await AsyncStorage.getItem("cartItems");
+    //items = JSON.parse(items);
+
+    const [productsID, setProductsID] = useState([]);
+    const cartsRef = doc(firestore, "carts", cartID);
+
+    useEffect(() => {
+      async function fetchCartData() {
+        const docSnap = await getDoc(cartsRef);
+        if (docSnap.exists()) {
+          console.log("Document data:", docSnap.data());
+        }
+        try {
+          console.log("try entered");
+          const productList = docSnap.get("productsID");
+          setProductsID(productList);
+        } catch (error) {
+          console.log("Error in finding products", error);
+        }
+      }
+      fetchCartData();
+    }, []); */
+/* remove item from cart button
+    <TouchableOpacity onPress={() => removeItemFromCart(product.id)}>
+              <Icon
+                name="delete-outline"
+                style={{
+                  fontSize: 16,
+                  color: COLORS.dark,
+                  backgroundColor: COLORS.light,
+                  padding: 8,
+                  borderRadius: 100,
+                }}
+              />
+            </TouchableOpacity>*/
