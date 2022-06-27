99cf78d3bd0cb152ce98e6efb4687842ed3a972b
fionlim <e0857418@u.nus.edu>
2022-06-27 20:23:58 +0800

    added more attributes to products

---
+++ screens/ShoppingScreenCopy.js
@@ -0,0 +1,355 @@
+//import { useNavigation } from '@react-navigation/core';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  StyleSheet,
+  Dimensions,
+  TextInput
+} from 'react-native';
+import COLORS from '../consts/colors';
+import products from '../consts/products';
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { useNavigation } from '@react-navigation/native';
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+const width = Dimensions.get("window").width / 2 - 30;
+
+const ShoppingScreen = () => {
+  const navigation = useNavigation();
+  const [catergoryIndex, setCategoryIndex] = React.useState(0);
+  const categories = ["POPULAR", "TOPS", "BOTTOMS", "DRESSES"];
+  const CategoryList = () => {
+    return (
+      <View style={styles.categoryContainer}>
+        {categories.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            activeOpacity={0.8}
+            onPress={() => setCategoryIndex(index)}
+          >
+            <Text
+              style={[
+                styles.categoryText,
+                catergoryIndex === index && styles.categoryTextSelected,
+              ]}
+            >
+              {item}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    );
+  };
+    
+    //create an product reusable card
+    const ProductCard = ({product}) => {
+        return (
+        <TouchableOpacity
+        onPress={() => navigation.navigate('Details', product)}
+        style={{
+          width: '48%',
+          marginVertical: 14,
+        }}>
+        <View
+          style={{
+            width: '100%',
+            height: 100,
+            borderRadius: 10,
+            backgroundColor: COLORS.backgroundLight,
+            position: 'relative',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: 8,
+          }}>
+          {product.isOff ? (
+            <View
+              style={{
+                position: 'absolute',
+                width: '20%',
+                height: '24%',
+                backgroundColor: COLORS.green,
+                top: 0,
+                left: 0,
+                borderTopLeftRadius: 10,
+                borderBottomRightRadius: 10,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: COLORS.white,
+                  fontWeight: 'bold',
+                  letterSpacing: 1,
+                }}>
+                {product.offPercentage}%
+              </Text>
+            </View>
+          ) : null}
+          <Image
+            source={product.img}
+            style={{
+              width: '80%',
+              height: '80%',
+              resizeMode: 'contain',
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            fontSize: 12,
+            color: COLORS.black,
+            fontWeight: '600',
+            marginBottom: 2,
+          }}>
+          {product.name}
+        </Text>
+        <Text>$ {product.price}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: COLORS.white,
+      }}>
+      <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            width: '100%',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            padding: 16,
+          }}>
+          <TouchableOpacity>
+            <Entypo
+              name="shopping-bag"
+              style={{
+                fontSize: 18,
+                color: COLORS.backgroundMedium,
+                padding: 12,
+                borderRadius: 10,
+                backgroundColor: COLORS.backgroundLight,
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('ShoppingCart')}>
+            <MaterialCommunityIcons
+              name="cart"
+              style={{
+                fontSize: 18,
+                color: COLORS.backgroundMedium,
+                padding: 12,
+                borderRadius: 10,
+                borderWidth: 1,
+                borderColor: COLORS.backgroundLight,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            marginBottom: 10,
+            padding: 16,
+          }}>
+          <Text
+            style={{
+              fontSize: 26,
+              color: COLORS.black,
+              fontWeight: '500',
+              letterSpacing: 1,
+              marginBottom: 10,
+            }}>
+            WELCOME
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              color: COLORS.black,
+              fontWeight: '400',
+              letterSpacing: 1,
+              lineHeight: 24,
+            }}>
+            Here to give you maximum savings
+            {'\n'}and an enjoyable shopping experience.
+          </Text>
+        </View>
+        <View
+          style={{
+            padding: 16,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+              <View
+        style={{ marginTop: 30, flexDirection: "row", paddingHorizontal: 50 }}
+      >
+        <View style={styles.searchContainer}>
+          <Icon name="search" size={25} style={{ marginLeft: 20 }} />
+          <TextInput placeholder="Search" style={styles.input} />
+        </View>
+        <View style={styles.sortBtn}>
+          <Icon name="sort" size={30} color={COLORS.white} />
+        </View>
+      </View>
+            <View>
+              <CategoryList/>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'space-around',
+            }}>
+          </View>
+        </View>
+
+        <View
+          style={{
+            padding: 16,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: COLORS.black,
+                  fontWeight: '400',
+                  opacity: 0.5,
+                  marginLeft: 10,
+                }}>
+                78
+              </Text>
+            </View>
+            <Text
+              style={{
+                fontSize: 14,
+                color: COLORS.blue,
+                fontWeight: '400',
+              }}>
+              SeeAll
+            </Text>
+          </View>
+          
+        </View>
+      </ScrollView>
+      <FlatList
+        columnWrapperStyle={{ justifyContent: "space-between" }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          marginTop: 10,
+          paddingBottom: 50,
+        }}
+        numColumns={2}
+        data={products}
+        renderItem={({ item }) => {
+          if (item.categoryno == catergoryIndex || catergoryIndex == 0) {
+            return <ProductCard product={item} />;
+          }
+          
+        }}
+      />
+    </View>
+  );
+};
+
+export default ShoppingScreen;
+
+const styles = StyleSheet.create({
+  categoryContainer: {
+    flexDirection: "row",
+    marginTop: 30,
+    justifyContent: "space-between",
+    marginBottom: 20,
+    paddingHorizontal: 300,
+  },
+  categoryText: { fontSize: 16, color: "grey", fontWeight: "bold" },
+  categoryTextSelected: {
+    color: COLORS.indigo,
+    paddingBottom: 5,
+    borderBottomWidth: 5,
+    borderColor: COLORS.indigo,
+  },
+  card: {
+    height: 225,
+    backgroundColor: COLORS.white,
+    width,
+    marginHorizontal: 2,
+    borderRadius: 10,
+    marginBottom: 20,
+    paddingHorizontal: 15,
+  },
+  header: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  searchContainer: {
+    height: 50,
+    backgroundColor: COLORS.white,
+    borderRadius: 10,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    fontSize: 18,
+    fontWeight: "bold",
+    flex: 1,
+    color: COLORS.dark,
+  },
+  sortBtn: {
+    marginLeft: 10,
+    height: 50,
+    width: 50,
+    borderRadius: 10,
+    backgroundColor: COLORS.indigo,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    width: "60%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 40,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
+
